fd7f35af29138d8b1f4066895e05f42c0fa68eaf
PenDZ-Alter <abxpendz45@gmail.com>
2026-08-19 02:39:06 +0700

    chore: moving some functions to headers

--- main.cpp
+++ main.cpp
@@ -7,38 +7,10 @@
 #include "src/parser.cpp"
 #include "src/params.h"
 #include "src/math.h"
+#include "src/func.h"
 
-void fft(std::vector<Complex>& x) {
-    size_t N = x.size();
-    if (N <= 1) return;
-
-    std::vector<Complex> even(N / 2), odd(N / 2);
-    for (size_t i = 0; i < N / 2; ++i) {
-        even[i] = x[i * 2];
-        odd[i]  = x[i * 2 + 1];
-    }
-
-    fft(even);
-    fft(odd);
-
-    for (size_t k = 0; k < N / 2; ++k) {
-        Complex t = std::polar(1.0, -2 * PI * k / N) * odd[k];
-        x[k]       = even[k] + t;
-        x[k + N/2] = even[k] - t;
-    }
-}
-
-// Mapping Frequency (Hz) to Pitch Class (0 = C, 1 = C#, ..., 11 = B)
-int freqToPitchClass(double freq) {
-    if (freq < 20.0 || freq > 5000.0) return -1; // Filter low & high freq (noise)
-    
-    // Formula MIDI Note
-    double pitch = 69.0 + 12.0 * std::log2(freq / 440.0);
-    int note = static_cast<int>(std::round(pitch)) % 12;
-    return (note < 0) ? note + 12 : note;
-}
-
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) 
+{
     std::string filePath = (argc > 1) ? argv[1] : std::string(SAMPLE_DIR) + "/sample.flac";
     
     std::cout << "Loading file: " << filePath << "...\n";
