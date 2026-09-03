6814e3dbf7eee6e90566f31472424cf3fc7e1f38
PenDZ-Alter <abxpendz45@gmail.com>
2026-09-03 22:24:35 +0700

    fix(build): re-define all shared libs on windows

--- src/api.h
+++ src/api.h
@@ -3,7 +3,8 @@
 #define API_H
 
 #if defined(_WIN32) || defined(__CYGWIN__)
-  #ifdef chordec_parser_EXPORTS // CMake otomatis buat macro <target_name>_EXPORTS saat build Shared
+// All Shared libraries on Windows should use __declspec(dllexport) to export symbols and __declspec(dllimport) to import them. This macro handles that automatically.
+  #if defined(chordec_parser_EXPORTS) || defined(chordec_gen_EXPORTS) || defined(chordec_math_EXPORTS)
     #define CHORDEC_API __declspec(dllexport)
   #else
     #define CHORDEC_API __declspec(dllimport)
