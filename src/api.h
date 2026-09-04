3a5a290d69fc71e6bf6f4fe9f7de1e756cdaee5a
PenDZ-Alter <abxpendz45@gmail.com>
2026-09-04 07:10:36 +0700

    build(test): adding static support in windows

--- src/api.h
+++ src/api.h
@@ -2,15 +2,19 @@
 #ifndef API_H
 #define API_H
 
-#if defined(_WIN32) || defined(__CYGWIN__)
-// All Shared libraries on Windows should use __declspec(dllexport) to export symbols and __declspec(dllimport) to import them. This macro handles that automatically.
-  #if defined(chordec_parser_EXPORTS) || defined(chordec_gen_EXPORTS) || defined(chordec_math_EXPORTS)
-    #define CHORDEC_API __declspec(dllexport)
-  #else
-    #define CHORDEC_API __declspec(dllimport)
-  #endif
+#if defined(CHORDEC_STATIC_BUILD)
+  #define CHORDEC_API
 #else
-  #define CHORDEC_API __attribute__((visibility("default")))
+  #if defined(_WIN32) || defined(__CYGWIN__)
+  // All Shared libraries on Windows should use __declspec(dllexport) to export symbols and __declspec(dllimport) to import them. This macro handles that automatically.
+    #if defined(chordec_parser_EXPORTS) || defined(chordec_gen_EXPORTS) || defined(chordec_math_EXPORTS)
+      #define CHORDEC_API __declspec(dllexport)
+    #else
+      #define CHORDEC_API __declspec(dllimport)
+    #endif
+  #else
+    #define CHORDEC_API __attribute__((visibility("default")))
+  #endif
 #endif
 
 #endif
